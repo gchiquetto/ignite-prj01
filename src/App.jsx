462dffb3ac00613cab10faeebc818db7b4409f44
Gabriela Chiquetto Policano <gabrielachiquetto@gmail.com>
2022-09-24 19:15:19 +0200

    added funcionality to post content

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,79 @@ import { Sidebar } from './components/Sidebar';
 import styles from './App.module.css';
 import './global.css';
 import { Post } from './components/Post';
+import { useState } from 'react';
 
 
-function App() {
+export function App() {
+  const posts = [
+    {
+      id: 1,
+      author: {
+        name: "Gabriela Chiquetto",
+        role: "Web Developer",
+        avatarSrc: "https://xesque.rocketseat.dev/users/avatar/profile-84ff1fc9-873b-4e89-b955-666ffc10c1d7-1663832191728.jpg",
+      },
+      date: new Date('2022-09-24T14:20:00'),
+      content:[
+        {
+          id:1,
+          type: "paragraph",
+          content: "I just uploaded one more project on my portfolio. It's a project that I made during the NLW Return"
+        },
+        {
+          id:2,
+          type: "link",
+          content: "👉 gabriela.design/doctorcare"
+        },
+        {
+          id:3,
+          type: "link",
+          content: "#newproject"
+        },
+        {
+          id:4,
+          type: "link",
+          content: "#nlw"
+        },
+        {
+          id:5,
+          type: "link",
+          content: "#rocketseat"
+        },
+      ]
+    },
+    {
+      id: 2,
+      author: {
+        name: "August Beckham",
+        role: "Designer",
+        avatarSrc: "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=200&q=40",
+      },
+      date: new Date('2022-09-23T08:20:00'),
+      content:[
+        {
+          id:1,
+          type: "paragraph",
+          content: "New project on the area!"
+        },
+        {
+          id:2,
+          type: "link",
+          content: "👾 abek.desing/greenphone"
+        },
+        {
+          id:3,
+          type: "link",
+          content: "#newproject"
+        },
+        {
+          id:4,
+          type: "link",
+          content: "#rocketseat"
+        },
+      ]
+    },
+  ];
 
   return (
     <div>
@@ -15,13 +85,10 @@ function App() {
       <div className={styles.wrapper}>
           <Sidebar />
           <main>
-            <Post />
-            <Post />
+            { posts.map( post =>  { return <Post key={post.id} content={post} /> })}
           </main>
 
       </div>
     </div>
   )
 }
-
-export default App
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,24 +1,33 @@
 import { Avatar } from './Avatar';
 import { Comment } from './Comment';
+import { format, formatDistance } from 'date-fns';
+
 import styles from './Post.module.css';
 
-export function Post(){
+export function Post( { content } ){
+
+    const date = new Date(content.date);
+    const dateFormatted = (formatDistance(date, new Date(), {addSuffix: true})).charAt(0).toUpperCase() + (formatDistance(date, new Date(), {addSuffix: true})).slice(1);
+
     return(
         <div className={styles.post}>
             <div className={styles.header}>
-                <Avatar src="https://xesque.rocketseat.dev/users/avatar/profile-84ff1fc9-873b-4e89-b955-666ffc10c1d7-1663832191728.jpg"/>
+                <Avatar src={ content.author.avatarSrc }/>
                 <div className={styles.information}>
-                    <strong>Gabriela Chiquetto</strong>
-                    <span>Web Developer</span>
+                    <strong>{ content.author.name }</strong>
+                    <span>{ content.author.role }</span>
                 </div>
-                <time dateTime='2022-09-23 14:00' title='September 23rd at 14:00'>Published 1 hour ago</time>
+                <time dateTime='2022-09-23 14:00' title='September 23rd at 14:00'>{dateFormatted}</time>
             </div>
             
             <div className={styles.content}>
-                <p>Hey guys 👋</p>
-                <p>I just uploaded one more project on my portfolio. It's a project that I made during the NLW Return</p>
-                <a href="">👉 gabriela.desing/doctorcare</a>
-                <span><a href="">#newproject</a> <a href="">#nlw</a> <a href="">#rocketseat</a></span>
+                {content.content.map(content => {
+                    if(content.type === 'paragraph'){
+                        return <p key={content.id}>{content.content}</p>
+                    } else if(content.type === 'link'){
+                        return <a key={content.id} href=''>{content.content}</a>
+                    }
+                })}
             </div>
 
             <footer style={styles.footer}>    
